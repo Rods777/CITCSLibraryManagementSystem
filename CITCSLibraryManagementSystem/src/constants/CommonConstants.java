968b2be54e05e9519d697e7eb4a7680679892ba0
Rods777 <rodelcuyag123@gmail.com>
2024-04-28 14:55:56 +0800

    Added Database and authentication to sign up

--- CITCSLibraryManagementSystem/src/constants/CommonConstants.java
+++ CITCSLibraryManagementSystem/src/constants/CommonConstants.java
@@ -16,4 +16,11 @@ public class CommonConstants {
 	public static final Color SAVE_BUTTON = Color.decode("#0060CC");
 	public static final Color EDIT_BUTTON = Color.decode("#0060CC");
 	public static final Color DELETE_BUTTON = Color.decode("#E74343");
+	
+	// Database Credentials
+	public static final String DB_JDBCDRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String DB_NAME = "citcslibrary";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
+	public static final String DB_USERNAME = "root";
+	public static final String DB_PASSWORD = "";
 }
